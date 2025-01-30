4602a80699e303f53eb8c1a3b7259ddd51ab41e0
Leo00rou <leolou.info@gmail.com>
2025-01-30 03:51:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // 1. Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xC6DEFF);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
