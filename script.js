6c897751508dc85badf1b2a92ac874930f338d0e
Leo00rou <leolou.info@gmail.com>
2025-01-30 15:31:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // 1. Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xC6DEFF);
+scene.background = null;
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById("three-container").appendChild(renderer.domElement);
 
 // 2. Load the texture
 const textureLoader = new THREE.TextureLoader();
@@ -35,7 +35,7 @@ const material = new THREE.MeshStandardMaterial({
     metalness: 0.1,
     roughness: 0.4,
     transparent: true,
-    opacity: 0.5  // Adjust transparency level
+    opacity: 0.8  // Adjust transparency level
 });
 
 const sphere = new THREE.Mesh(geometry, material);
